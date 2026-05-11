cec5d402f100e6cf9ea4360ef54859803f5d46e3
PxrpGill <jhongil3007@gmail.com>
2026-05-11 12:06:16 +0300

    feature: настроить css

--- src/postcss.config.mjs
+++ src/postcss.config.mjs
@@ -1,5 +1,26 @@
 const config = {
-  plugins: {},
+	plugins: {
+		"postcss-import": {
+			path: [path.resolve(__dirname, "shared/styles")],
+		},
+		"postcss-mixins": {
+			mixinsDir: path.resolve(__dirname, "shared/styles/mixins"),
+		},
+		"postcss-simple-vars": {
+			variables: {
+				"mobile-min": "375px",
+				mobile: "767px",
+
+				"desktop-min": "768px",
+				desktop: "1440px",
+
+				"mobile-scale-factor": "3.75",
+				"standard-desktop-scale-factor": "14.4",
+			},
+		},
+		"postcss-nested": {},
+		autoprefixer: {},
+	},
 };
 
 export default config;
